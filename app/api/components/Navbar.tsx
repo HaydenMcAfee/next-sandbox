e7efacfd92f3fe3641cdcbe4fdf3c70e55da3ec2
Haydn <haydenmac24@gmail.com>
2026-02-13 11:10:55 -0500

    cleanup

--- app/api/components/Navbar.tsx
+++ app/api/components/Navbar.tsx
@@ -5,7 +5,6 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import {
-    Link,
     Stack,
 } from '@mui/material';
 import CloudQueueIcon from '@mui/icons-material/CloudQueue';
